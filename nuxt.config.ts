0a0278a9b39e3d938d725cffe1bc630cab8d443a
Aleix Anguita Gomez <aleixango@gmail.com>
2023-08-09 20:28:27 +0200

    Added linter, prettier and tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  modules: ["@nuxthq/ui"],
+  devtools: { enabled: false },
+  css: ["@/assets/css/tailwind.css"],
+  postcss: {
+    plugins: {
+      autoprefixer: {},
+      tailwindcss: {},
+    },
+  },
+});
