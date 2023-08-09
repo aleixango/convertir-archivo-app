578fb76561c1179e7450101c7fa899492d7830dd
Aleix Anguita Gomez <aleixango@gmail.com>
2023-08-09 20:28:58 +0200

    Added simple landing page

--- app.vue
+++ app.vue
@@ -1,5 +1,40 @@
+<script setup lang="ts">
+const colorMode = useColorMode();
+const isDark = computed({
+  get() {
+    return colorMode.preference === "dark";
+  },
+  set() {
+    colorMode.preference = colorMode.value === "dark" ? "light" : "dark";
+  },
+});
+</script>
+
 <template>
-  <header>
-    <h1>Convertir Archivo</h1>
-  </header>
+  <div class="mx-auto grid min-h-screen max-w-screen-xl grid-rows-[60px,1fr,60px] px-6">
+    <header class="flex items-center">
+      <nav class="flex-1">
+        <a class="text-2xl" href="/"><b>Convertir</b> Archivo</a>
+      </nav>
+      <UButton
+        class="flex-none"
+        variant="soft"
+        aria-label="Theme"
+        :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
+        @click="isDark = !isDark"
+      ></UButton>
+    </header>
+    <main>
+      <NuxtPage></NuxtPage>
+    </main>
+    <footer class="flex items-center justify-center">
+      <p>Made with ❤️ by Aleix Anguita</p>
+    </footer>
+  </div>
 </template>
+
+<style>
+body {
+  color-scheme: dark light;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex flex-col items-center">
+    <h1
+      class="mt-14 flex flex-col text-center text-6xl font-bold tracking-tight md:mt-28 md:flex-row md:text-6xl lg:text-7xl xl:text-8xl"
+    >
+      <span>Copia</span>
+      <span class="hidden md:inline">&blacktriangleright;</span>
+      <span class="-mb-4 mt-1 inline md:hidden">&blacktriangledown;</span>
+      <span>Pega</span>
+      <span class="hidden md:inline">&blacktriangleright;</span>
+      <span class="-mb-4 mt-1 inline md:hidden">&blacktriangledown;</span>
+      <span>Convierte</span>
+    </h1>
+    <h2
+      class="mx-0 mt-8 max-w-lg text-center text-base md:mx-44 md:mt-14 md:max-w-none lg:mx-52 lg:text-xl xl:mx-36 xl:text-2xl"
+    >
+      Convertir Archivo ayuda a profesionales a transformar texto y archivos entre diferentes formatos, aumentado su
+      productividad
+    </h2>
+    <NuxtLink
+      to="/convertir"
+      class="bg-primary-500 hover:bg-primary-600 mt-10 rounded-md px-8 py-3 text-xl md:text-2xl"
+    >
+      Empezar
+    </NuxtLink>
+  </div>
+</template>
